27f28fbf1a5ebf7d22ae48644d1b52ac5fcfc604
surbhi <surbhisharma.edunext@gmail.com>
2025-10-30 16:56:19 +0530

    update

--- src/firstcomponent/Header.jsx
+++ src/firstcomponent/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Header(){
     return(
+        <div style={{backgroundColor: 'green',width: '700px',textAlign:'left',marginTop:'50px'}}>
         <h1>header part</h1>
+        </div>
     );
 }
 
